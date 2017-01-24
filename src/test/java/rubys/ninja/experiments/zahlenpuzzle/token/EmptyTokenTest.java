8dcbcf15a661dd770ea165de2f064b88fb0d111f
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-01-24 14:25:10 +0100

    Implement empty token

---
+++ src/test/java/rubys/ninja/experiments/zahlenpuzzle/token/EmptyTokenTest.java
@@ -0,0 +1,17 @@
+package rubys.ninja.experiments.zahlenpuzzle.token;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertSame;
+
+/**
+ * @author Ruben Schmidmeister (bash)
+ */
+public class EmptyTokenTest {
+
+    @Test
+    public void testInstatiationWorks() {
+        Token token = new EmptyToken();
+    }
+
+}
