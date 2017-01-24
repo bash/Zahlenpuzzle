74a764606c445025bc91562df3df9a60623fe22f
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-01-24 17:00:39 +0100

    Write Board Tests

--- src/main/java/rubys/ninja/experiments/zahlenpuzzle/board/Board.java
+++ src/main/java/rubys/ninja/experiments/zahlenpuzzle/board/Board.java
@@ -1,4 +1,19 @@
 package rubys.ninja.experiments.zahlenpuzzle.board;
 
+import rubys.ninja.experiments.zahlenpuzzle.token.Token;
+
 public class Board {
+    private BoardSize size;
+
+    public Board(BoardSize size) {
+        this.size = size;
+    }
+
+    public Token getTokenAt(BoardSize location) {
+        return null;
+    }
+
+    public BoardSize getSize() {
+        return size;
+    }
 }
--- src/test/java/ruby/ninja/experiments/zahlenpuzzle/board/BoardTest.java
+++ src/test/java/ruby/ninja/experiments/zahlenpuzzle/board/BoardTest.java
@@ -1,5 +1,49 @@
 package ruby.ninja.experiments.zahlenpuzzle.board;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import rubys.ninja.experiments.zahlenpuzzle.board.Board;
+import rubys.ninja.experiments.zahlenpuzzle.board.BoardSize;
+import rubys.ninja.experiments.zahlenpuzzle.token.Token;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
 public class BoardTest {
-    
+    private Board board;
+
+    @Before
+    public void setup() throws Exception {
+        board = new Board(new BoardSize(4, 5));
+    }
+
+    @Test
+    public void TestGetSize() throws Exception {
+        assertEquals(new BoardSize(4, 5), board.getSize());
+    }
+
+    @Test
+    public void TestGetToken() throws Exception {
+        Token token = board.getTokenAt(new BoardSize(3, 3));
+        assertNotNull(token);
+    }
+
+    @Test(expected = Exception.class)
+    public void TestBounds() throws Exception {
+        Token token = board.getTokenAt(new BoardSize(5, 6));
+    }
+
+    @Test
+    public void TestSwapWithPos() throws Exception {
+        BoardSize pos1 = new BoardSize(0, 0);
+        BoardSize pos2 = new BoardSize(0, 1);
+
+        Token oldToken1 = board.getTokenAt(pos1);
+
+        board.swapTokens(pos1, pos2);
+
+
+    }
 }
