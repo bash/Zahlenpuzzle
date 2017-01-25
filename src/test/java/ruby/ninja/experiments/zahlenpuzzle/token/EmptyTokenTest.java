dd760aa06e20ee2132c1b6792b59904c119938c2
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-01-25 21:39:38 +0100

    Remove redundant import

--- src/test/java/ruby/ninja/experiments/zahlenpuzzle/token/EmptyTokenTest.java
+++ src/test/java/ruby/ninja/experiments/zahlenpuzzle/token/EmptyTokenTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import rubys.ninja.experiments.zahlenpuzzle.token.EmptyToken;
 import rubys.ninja.experiments.zahlenpuzzle.token.Token;
 
-import static org.junit.Assert.assertSame;
-
 public class EmptyTokenTest {
     @Test
     public void testInstantiationWorks() {
