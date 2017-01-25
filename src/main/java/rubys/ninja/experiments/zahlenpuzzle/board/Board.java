f58155c14377781e4fc985192dc7618cdbaf6fa7
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-01-25 21:52:02 +0100

    Implement swapTokens, getTokenAt

--- src/main/java/rubys/ninja/experiments/zahlenpuzzle/board/Board.java
+++ src/main/java/rubys/ninja/experiments/zahlenpuzzle/board/Board.java
@@ -14,7 +14,9 @@ public class Board {
     }
 
     public Token getTokenAt(BoardSize location) throws Exception {
-        return null;
+        int index = getIndexFromLocation(location);
+
+        return tokens.get(index);
     }
 
     public BoardSize getSize() {
@@ -22,5 +24,17 @@ public class Board {
     }
 
     public void swapTokens(BoardSize pos1, BoardSize pos2) throws Exception {
+        Token token1 = getTokenAt(pos1);
+        Token token2 = getTokenAt(pos2);
+
+        int index1 = getIndexFromLocation(pos1);
+        int index2 = getIndexFromLocation(pos2);
+
+        tokens.set(index1, token2);
+        tokens.set(index2, token1);
+    }
+
+    private int getIndexFromLocation(BoardSize location) {
+        return location.getHeight() * size.getWidth() + location.getWidth();
     }
 }
