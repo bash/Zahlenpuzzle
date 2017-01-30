1233765e9a670728fb4f5d2bf93cdcbfca563469
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-01-30 20:54:27 +0100

    Implement isAllowedToSwap in OneEmptyAndSortedNumericRuleEnforcer

--- src/main/java/rubys/ninja/experiments/zahlenpuzzle/game/OneEmptyAndSortedNumericRuleEnforcer.java
+++ src/main/java/rubys/ninja/experiments/zahlenpuzzle/game/OneEmptyAndSortedNumericRuleEnforcer.java
@@ -2,11 +2,22 @@ package rubys.ninja.experiments.zahlenpuzzle.game;
 
 import rubys.ninja.experiments.zahlenpuzzle.board.Board;
 import rubys.ninja.experiments.zahlenpuzzle.board.BoardSize;
+import rubys.ninja.experiments.zahlenpuzzle.token.EmptyToken;
+import rubys.ninja.experiments.zahlenpuzzle.token.Token;
 
 
 public class OneEmptyAndSortedNumericRuleEnforcer implements RuleEnforcer {
     public boolean isAllowedToSwap(Board board, BoardSize pos1, BoardSize pos2) {
-        return false;
+        Token token1 = board.getTokenAt(pos1);
+        Token token2 = board.getTokenAt(pos2);
+
+        boolean token1IsEmpty = token1 instanceof EmptyToken;
+        boolean token2IsEmpty = token2 instanceof EmptyToken;
+
+        boolean areAdjacent = board.areAdjacent(pos1, pos2);
+        boolean oneIsEmpty = token1IsEmpty != token2IsEmpty;
+
+        return areAdjacent && oneIsEmpty;
     }
 
     public boolean isFinished(Board finishedBoard) {
