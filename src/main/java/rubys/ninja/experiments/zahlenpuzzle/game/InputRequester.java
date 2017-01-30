ce1f9c357e70dfc206f378eb4b01106ce8930739
SirRade <jn_ferner@hotmail.de>
2017-01-30 23:14:06 +0100

    Remove the Ferner

--- src/main/java/rubys/ninja/experiments/zahlenpuzzle/game/InputRequester.java
+++ src/main/java/rubys/ninja/experiments/zahlenpuzzle/game/InputRequester.java
@@ -2,9 +2,6 @@ package rubys.ninja.experiments.zahlenpuzzle.game;
 
 import rubys.ninja.experiments.zahlenpuzzle.board.BoardSize;
 
-/**
- * Created by jn_fe on 30.01.2017.
- */
 public interface InputRequester {
     class SwapRequest {
         public BoardSize pos1;
