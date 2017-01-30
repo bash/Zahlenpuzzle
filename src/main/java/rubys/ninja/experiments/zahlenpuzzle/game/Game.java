2115a8888e09e84c9ef81e4c90e3fbf6ed3e1589
SirRade <jn_ferner@hotmail.de>
2017-01-30 23:05:25 +0100

    Add Game

--- src/main/java/rubys/ninja/experiments/zahlenpuzzle/game/Game.java
+++ src/main/java/rubys/ninja/experiments/zahlenpuzzle/game/Game.java
@@ -1,5 +1,15 @@
 package rubys.ninja.experiments.zahlenpuzzle.game;
 
-public interface Game {
+import rubys.ninja.experiments.zahlenpuzzle.board.Board;
 
+public interface Game {
+    enum UpdateResult {
+        SuccessfulSwap,
+        RequestDenied,
+        GameFinished
+    }
+    Game.UpdateResult update();
+    Board getBoard();
+    boolean hasWon();
+    boolean isFinished();
 }
