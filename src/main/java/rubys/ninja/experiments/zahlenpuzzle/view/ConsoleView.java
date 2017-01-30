7c175af0c6a90484ae4d97b7781203d70f6c4977
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-01-31 00:56:28 +0100

    Add clear screen

--- src/main/java/rubys/ninja/experiments/zahlenpuzzle/view/ConsoleView.java
+++ src/main/java/rubys/ninja/experiments/zahlenpuzzle/view/ConsoleView.java
@@ -35,6 +35,8 @@ public class ConsoleView {
     }
 
     private void printBoard(Board board) {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
         System.out.println(renderBoard(board));
     }
 
